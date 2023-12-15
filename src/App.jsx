a447e3528e8f0520ff9b27217d40795bef5ec19e
Vasco Godinho <vascogodinho88@live.com.pt>
2023-12-15 00:00:46 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,84 +8,100 @@ import studentsData from "./assets/students.json";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+  const [fullName, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [program, setProgram] = useState("");
+  const [image, setImage] = useState("");
+  const [graduationYear, setGraduationYear] = useState(2023);
+  const [graduated, setGraduated] = useState(false);
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
 
-  return (
-    <div className="App pt-20">
-      <Navbar />
+    const newStudent = {
+      fullName,
+      image,
+      phone,
+      email,
+      program,
+      graduationYear,
+      graduated,
+    };
 
-      {/* FORM */}
-      <form>
-        <span>Add a Student</span>
-        <div>
-          <label>
-            Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
-          </label>
+    return (
+      <div className="App pt-20">
+        <Navbar />
 
-          <label>
-            Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
-          </label>
+        {/* FORM */}
+        <form onSubmit={handleFormSubmit}>
+          <span>Add a Student</span>
+          <div>
+            <label>
+              Full Name
+              <input name="fullName" type="text" placeholder="Full Name" />
+            </label>
 
-          <label>
-            Phone
-            <input name="phone" type="tel" placeholder="Phone" />
-          </label>
+            <label>
+              Profile Image
+              <input name="image" type="url" placeholder="Profile Image" />
+            </label>
 
-          <label>
-            Email
-            <input name="email" type="email" placeholder="Email" />
-          </label>
-        </div>
+            <label>
+              Phone
+              <input name="phone" type="tel" placeholder="Phone" />
+            </label>
 
-        <div>
-          <label>
-            Program
-            <select name="program">
-              <option value="">-- None --</option>
-              <option value="Web Dev">Web Dev</option>
-              <option value="UXUI">UXUI</option>
-              <option value="Data">Data</option>
-            </select>
-          </label>
+            <label>
+              Email
+              <input name="email" type="email" placeholder="Email" />
+            </label>
+          </div>
 
-          <label>
-            Graduation Year
-            <input
-              name="graduationYear"
-              type="number"
-              placeholder="Graduation Year"
-              minLength={4}
-              maxLength={4}
-              min={2023}
-              max={2030}
-            />
-          </label>
+          <div>
+            <label>
+              Program
+              <select name="program">
+                <option value="">-- None --</option>
+                <option value="Web Dev">Web Dev</option>
+                <option value="UXUI">UXUI</option>
+                <option value="Data">Data</option>
+              </select>
+            </label>
 
-          <label>
-            Graduated
-            <input name="graduated" type="checkbox" />
-          </label>
+            <label>
+              Graduation Year
+              <input
+                name="graduationYear"
+                type="number"
+                placeholder="Graduation Year"
+                minLength={4}
+                maxLength={4}
+                min={2023}
+                max={2030}
+              />
+            </label>
 
-          <button type="submit">Add Student</button>
-        </div>
+            <label>
+              Graduated
+              <input name="graduated" type="checkbox" />
+            </label>
 
-      </form>
-      {/* FORM END */}
+            <button type="submit">Add Student</button>
+          </div>
+        </form>
+        {/* FORM END */}
 
+        {/* TABLE/LIST HEADER */}
+        <TableHeader />
 
-      {/* TABLE/LIST HEADER */}
-      <TableHeader />
-
-
-      {/* STUDENT LIST */}
-      {students &&
-        students.map((student) => {
-          return <StudentCard key={student.email} {...student} />;
-        })}
-    </div>
-  );
+        {/* STUDENT LIST */}
+        {students &&
+          students.map((student) => {
+            return <StudentCard key={student.email} {...student} />;
+          })}
+      </div>
+    );
+  };
 }
-
 export default App;
